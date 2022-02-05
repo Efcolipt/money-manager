1349163cbc3cb48a20fc47b7b44b5f04cc89b636
Ivan <45362427+Efcolipt@users.noreply.github.com>
2022-02-05 16:24:03 +0300

    add header & footer for profile

---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header id="header" class="py-8">
+        <div class="container mx-auto px-8">
+            <div class="flex justify-between items-center">
+                <div class="flex items-center">
+                    <div class="w-10 h-10 overflow-hidden rounded-full">
+                        <img
+                            class="w-full h-auto object-cover"
+                            src="https://i.pinimg.com/736x/d8/34/89/d834897e4d0849e1f897de1b580f376d.jpg"
+                            alt="User profile Avatar"
+                            size="sm"
+                        />
+                    </div>
+                    <h1 class="text-xl font-bold text-dark ml-2"></h1>
+                </div>
+                <div class="flex">
+                    <ul>
+                        <li>
+                            <i class="t2ico t2ico-notification"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    setup() {},
+};
+</script>
+
+<style scoped lang="css"></style>
