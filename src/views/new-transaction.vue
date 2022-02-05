9720420c693f24444e00d410c57270238e5bf9ed
Ivan <45362427+Efcolipt@users.noreply.github.com>
2022-02-06 02:22:58 +0300

    Create a new Transaction

---
+++ src/views/new-transaction.vue
@@ -0,0 +1,211 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <!-- : Form main -->
+        <div class="row mt-8">
+            <div class="bg-white rounded-lg py-4">
+                <div class="container mx-auto px-8">
+                    <div class="row">
+                        <label for="total" class="flex items-end">
+                            <div class="w-10 text-right leading-10 mr-4 pb-1">
+                                <span
+                                    class="inline-block px-1 text-dark border border-dark rounded text-sm"
+                                >
+                                    USD
+                                </span>
+                            </div>
+                            <div
+                                class="flex flex-col border-b border-gray-100 pb-1"
+                            >
+                                <span class="font-semibold text-xs text-dark"
+                                    >Total</span
+                                >
+                                <input
+                                    type="text"
+                                    id="total"
+                                    class="text-4xl text-dark w-full outline-none mt-1"
+                                    placeholder="0"
+                                />
+                            </div>
+                        </label>
+                    </div>
+                    <div class="row">
+                        <label for="category" class="flex items-center">
+                            <div
+                                class="flex items-center flex-none w-10 text-right leading-10 mr-4"
+                            >
+                                <span
+                                    class="inline-block ml-auto w-8 h-8 rounded-full bg-blue-300"
+                                ></span>
+                            </div>
+                            <div class="flex-1 border-b border-gray-100 py-3">
+                                <input
+                                    type="text"
+                                    id="category"
+                                    class="text-xl text-dark w-full outline-none"
+                                    placeholder="Select a category"
+                                />
+                            </div>
+                        </label>
+                    </div>
+                    <div class="row">
+                        <label for="note" class="flex items-center">
+                            <div
+                                class="flex items-center flex-none w-10 text-right leading-10 mr-4"
+                            >
+                                <span class="flex-none w-10 mr-4">
+                                    <i
+                                        class="t2ico t2ico-document text-2xl"
+                                    ></i>
+                                </span>
+                            </div>
+                            <div class="flex-1 border-b border-gray-100 py-3">
+                                <input
+                                    type="text"
+                                    id="note"
+                                    class="text-dark w-full outline-none"
+                                    placeholder="Note"
+                                />
+                            </div>
+                        </label>
+                    </div>
+                    <div class="row">
+                        <label for="createdAt" class="flex items-center">
+                            <div class="flex-none w-10 mr-4">
+                                <span
+                                    class="flex items-center justify-end text-dark"
+                                >
+                                    <i
+                                        class="t2ico t2ico-calendar text-2xl"
+                                    ></i>
+                                </span>
+                            </div>
+                            <div class="flex-1 py-2 border-b border-gray-100">
+                                <div class="text-dark w-full">
+                                    Sun, 29 Dec 2021
+                                </div>
+                            </div>
+                        </label>
+                    </div>
+                    <div class="row">
+                        <label for="wallet" class="flex items-center">
+                            <div class="flex-none w-10 mr-4">
+                                <span
+                                    class="flex items-center justify-end text-dark"
+                                >
+                                    <i class="t2ico t2ico-wallet text-2xl"></i>
+                                </span>
+                            </div>
+                            <div class="flex-1 py-2">
+                                <div class="text-dark w-full">My Wallet</div>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-8" v-if="!isMoreDetails">
+            <button
+                @click="isMoreDetails = true"
+                class="bg-white rounded-lg py-3 w-full text-primary font-semibold"
+            >
+                More Details
+            </button>
+        </div>
+
+        <!-- : Form advanced -->
+        <template v-if="isMoreDetails">
+            <div class="row mt-8">
+                <div class="bg-white rounded-lg py-4">
+                    <div class="container mx-auto px-8">
+                        <div class="row">
+                            <label for="location" class="flex items-center">
+                                <div
+                                    class="flex items-center flex-none w-10 text-right leading-10 mr-4"
+                                >
+                                    <span class="flex-none w-10 mr-4">
+                                        <i
+                                            class="t2ico t2ico-location text-2xl"
+                                        ></i>
+                                    </span>
+                                </div>
+                                <div
+                                    class="flex-1 border-b border-gray-100 py-3"
+                                >
+                                    <input
+                                        type="text"
+                                        id="location"
+                                        class="text-dark w-full outline-none"
+                                        placeholder="location"
+                                    />
+                                </div>
+                            </label>
+                        </div>
+                        <div class="row">
+                            <label for="withPerson" class="flex items-center">
+                                <div class="flex-none w-10 mr-4">
+                                    <span
+                                        class="flex items-center justify-end text-dark"
+                                    >
+                                        <i
+                                            class="t2ico t2ico-users text-2xl"
+                                        ></i>
+                                    </span>
+                                </div>
+                                <div class="flex-1 py-2">
+                                    <input
+                                        type="text"
+                                        id="withPerson"
+                                        class="text-dark w-full outline-none"
+                                        placeholder="With person"
+                                    />
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+
+        <!-- : Upload photos -->
+        <div class="row mt-8">
+            <div class="bg-white rounded-lg py-4">
+                <div class="container mx-auto px-8">
+                    <div class="row">
+                        <label
+                            for="camera"
+                            class="flex items-center text-primary"
+                        >
+                            <div class="flex-none w-10 mr-4">
+                                <span
+                                    class="flex items-center justify-end text-dark"
+                                >
+                                    <i
+                                        class="t2ico t2ico-camera text-2xl text-primary"
+                                    ></i>
+                                </span>
+                            </div>
+                            <div class="flex-1 py-2">
+                                <div class="w-ful">Upload Photos</div>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const isMoreDetails = ref(false);
+
+        function onSubmit() {}
+
+        return { onSubmit, isMoreDetails };
+    },
+};
+</script>
